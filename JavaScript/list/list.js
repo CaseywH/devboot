3514e1b25e2ef29a417cb6a4ac47fa6827bd52da
Casey Heinemeyer <doulos1129@gmail.com>
2017-06-05 21:12:12 -0700

    complete

--- JavaScript/list/list.js
+++ JavaScript/list/list.js
@@ -4,18 +4,33 @@ var input = prompt("What do you want to do?");
 
 while (input !== "quit") {
   if (input === "list") {
-    console.log("**********");
-    todos.forEach(function(todo, i){
-        console.log(i + ": " + todo);
-    });
-    console.log("**********");
+    listTodos();
   } else if (input === "new") {
-    var newTodo = prompt("Enter new Todo");
-    todos.push(newTodo);
+    addTodo();
   } else if (input === "delete") {
-    var index = prompt("enter index of todo to delete");
-    todos.splice(index, 1);
+    deleteTodo();
   }
   input = prompt("What do you want to do?");
 }
 console.log("OK, you quit the app");
+
+
+function listTodos() {
+  console.log("**********");
+  todos.forEach(function(todo, i){
+      console.log(i + ": " + todo);
+  });
+  console.log("**********");
+}
+
+function addTodo() {
+  var newTodo = prompt("Enter new Todo");
+  todos.push(newTodo);
+  console.log("added todo");
+}
+
+function deleteTodo() {
+  var index = prompt("enter index of todo to delete");
+  todos.splice(index, 1);
+  console.log("deleted todo");
+}
