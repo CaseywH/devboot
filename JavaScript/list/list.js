2a7eef5080319c4d089c39ac454927bfafa5bdf5
Casey Heinemeyer <doulos1129@gmail.com>
2017-06-05 21:01:42 -0700

    working

--- JavaScript/list/list.js
+++ JavaScript/list/list.js
@@ -2,12 +2,20 @@ var todos = [];
 
 var input = prompt("What do you want to do?");
 
-while (input != "qut") {
+while (input !== "quit") {
   if (input === "list") {
-    console.log(todos);
+    console.log("**********");
+    todos.forEach(function(todo, i){
+        console.log(i + ": " + todo);
+    });
+    console.log("**********");
   } else if (input === "new") {
     var newTodo = prompt("Enter new Todo");
     todos.push(newTodo);
+  } else if (input === "delete") {
+    var index = prompt("enter index of todo to delete");
+    todos.splice(index, 1);
   }
+  input = prompt("What do you want to do?");
 }
 console.log("OK, you quit the app");
