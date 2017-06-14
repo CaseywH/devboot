aae054fd646dbdb38b75e8cf8923321bb23af436
Casey Heinemeyer <doulos1129@gmail.com>
2017-06-13 20:52:54 -0700

    process

---
+++ DOMmanipulation/DOM-intro/process.js
@@ -0,0 +1,9 @@
+
+
+var h1 = document.querySelector("h1");
+
+var li = document.querySelector("#highlight");
+
+var li = document.querySelector('.bolded');
+
+var lis = document.querySelectorAll(".bolded");
