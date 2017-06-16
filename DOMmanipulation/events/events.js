9be9293118de8273e373fa6da9d0557a53dfc600
Casey Heinemeyer <doulos1129@gmail.com>
2017-06-15 21:00:10 -0700

    events

---
+++ DOMmanipulation/events/events.js
@@ -0,0 +1,19 @@
+
+var button = document.querySelector("button");
+var isPurple = false;
+
+// button.addEventListener("click", function() {
+//   //if white make it purple
+//   if (isPurple){
+//   document.body.style.background = "white";
+//
+// } else {
+//   document.body.style.background = "purple";
+//
+// }
+// isPurple = !isPurple;
+// });
+
+button.addEventListener("click", function() {
+  document.body.classList.toggle("purple");
+});
