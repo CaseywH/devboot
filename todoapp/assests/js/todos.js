94329adaa341b31191f82e1941ff4f3c90009b93
Casey Heinemeyer <doulos1129@gmail.com>
2017-06-25 20:38:32 -0700

    gradient

--- todoapp/assests/js/todos.js
+++ todoapp/assests/js/todos.js
@@ -1,7 +1,24 @@
 // check off specific todos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   //if li is gray
 $(this).toggleClass("completed");
 
+});
+// click on x to delete todos
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13){
+    //grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
